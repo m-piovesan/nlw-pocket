afee16cc839f254124392d27ad248c4370f33650
m-piovesan <matheusmoraespiovesan@gmail.com>
2024-09-12 08:44:11 -0300

    fixed the last details on server and cgs.ts

--- server/src/functions/create-goal-completion.ts
+++ server/src/functions/create-goal-completion.ts
@@ -1,6 +1,6 @@
-import { and, count, gte, lte } from "drizzle-orm";
+import { and, count, eq, gte, lte, sql } from "drizzle-orm";
 import { db } from "../db";
-import { goalCompletions } from "../db/schema";
+import { goalCompletions, goals } from "../db/schema";
 import dayjs from "dayjs";
 
 interface CreateGoalCompletionRequest {
@@ -8,7 +8,9 @@ interface CreateGoalCompletionRequest {
 }
 
 // This function is responsible for creating a goal completion in the database and returning the created goal completion
-export async function createGoal({ goalId }: CreateGoalCompletionRequest) {
+export async function createGoalCompletion({
+  goalId,
+}: CreateGoalCompletionRequest) {
   const firstDayOfTheWeek = dayjs().startOf("week").toDate();
   const lastDayOfTheWeek = dayjs().endOf("week").toDate();
 
@@ -22,18 +24,39 @@ export async function createGoal({ goalId }: CreateGoalCompletionRequest) {
       .where(
         and(
           lte(goalCompletions.createdAt, lastDayOfTheWeek),
-          gte(goalCompletions.createdAt, firstDayOfTheWeek)
+          gte(goalCompletions.createdAt, firstDayOfTheWeek),
+          eq(goalCompletions.goalId, goalId)
         )
       )
       .groupBy(goalCompletions.goalId)
   );
 
   const result = await db
+    .with(goalsCompletionCount)
+    .select({
+      desiredWeeklyFrequency: goals.desiredWeeklyFrequency,
+      completionCount:
+        sql`COALESCE(${goalsCompletionCount.completionCount}, 0)`.mapWith(
+          Number
+        ),
+    })
+    .from(goals)
+    .leftJoin(goalsCompletionCount, eq(goalsCompletionCount.goalId, goals.id))
+    .where(eq(goals.id, goalId))
+    .limit(1);
+
+  const { completionCount, desiredWeeklyFrequency } = result[0];
+
+  if (completionCount >= desiredWeeklyFrequency) {
+    throw new Error("Goal already completed for the week");
+  }
+
+  const insertResult = await db
     .insert(goalCompletions)
     .values({ goalId })
     .returning();
 
-  const goalCompletion = result[0];
+  const goalCompletion = insertResult[0];
 
   return {
     goalCompletion,
--- server/src/http/server.ts
+++ server/src/http/server.ts
@@ -7,12 +7,14 @@ import {
 import { createGoal } from "../functions/create-goal";
 import z from "zod";
 import { getWeekPendingGoals } from "../functions/get-week-pending-goals";
+import { createGoalCompletion } from "../functions/create-goal-completion";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
+// returns the pending goals for the week
 app.get("/pending-goals", async () => {
   const { pendingGoals } = await getWeekPendingGoals();
 
@@ -21,6 +23,7 @@ app.get("/pending-goals", async () => {
   };
 });
 
+// creates a new goal
 app.post(
   "/goals",
   {
@@ -37,6 +40,24 @@ app.post(
     await createGoal({
       title,
       desiredWeeklyFrequency,
+    });
+  }
+);
+
+app.post(
+  "completions",
+  {
+    schema: {
+      body: z.object({
+        goalId: z.string(),
+      }),
+    },
+  },
+  async (request) => {
+    const { goalId } = request.body;
+
+    await createGoalCompletion({
+      goalId,
     });
   }
 );
